d21276a6bed59dc51f86677262708894474fd3a6
Nick Putnam <putnam3d@gmail.com>
2024-08-13 11:18:28 -0500

    updated App.js to use Drei threejs framework and updated package.json dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Box, Text } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas style={{ height: "100vh" }}>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+
+      {/* Add a rotating Box */}
+      <Box args={[1, 1, 1]} position={[-2, 0, 0]}>
+        <meshStandardMaterial attach="material" color="blue" />
+      </Box>
+
+      {/* Add some text */}
+      <Text position={[2, 0, 0]} fontSize={0.5} color="white">
+        Hello R3F
+      </Text>
+
+      <OrbitControls />
+    </Canvas>
   );
 }
 
 export default App;
+
