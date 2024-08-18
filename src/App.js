c256bbfb001df201af5058bb9fd99c52a875bf00
Nick Putnam <putnam3d@gmail.com>
2024-08-18 12:17:57 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import Navbar from "./components/NavMenu/Navbar/Navbar";
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Box, Text } from '@react-three/drei';
+import './App.css';
 
 function App() {
   return (
-    <Canvas style={{ height: "100vh" }}>
-      <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
+    <div className="App">
+      <Navbar />
 
-      {/* Add a rotating Box */}
-      <Box args={[1, 1, 1]} position={[-2, 0, 0]}>
-        <meshStandardMaterial attach="material" color="blue" />
-      </Box>
+      <Canvas style={{ height: "100vh" }}>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
 
-      {/* Add some text */}
-      <Text position={[2, 0, 0]} fontSize={0.5} color="white">
-        Hello R3F
-      </Text>
+        {/* Add a rotating Box */}
+        <Box args={[1, 1, 1]} position={[-2, 0, 0]}>
+          <meshStandardMaterial attach="material" color="blue" />
+        </Box>
 
-      <OrbitControls />
-    </Canvas>
+        {/* Add some text */}
+        <Text position={[2, 0, 0]} fontSize={0.5} color="white">
+          Hello R3F
+        </Text>
+
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 }
 
---
+++ src/components/NavMenu/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Import the CSS for styling
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Unite Byte</div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
