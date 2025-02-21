e1951a70a80af70d07041358c8f62deda69f69d1
Nick Putnam <putnam3d@gmail.com>
2025-02-20 21:12:50 -0600

    Initial Commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button(){
+    return (
+        <button style={{ padding: "10px 20px", background: "blue", color: "white"}}>
+            Click Me
+        </button>
+    );
+}
