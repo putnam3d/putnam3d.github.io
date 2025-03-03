8272ff6a1e5f9004b864cae9aa16153540ccbc99
Nick Putnam <putnam3d@gmail.com>
2025-03-02 18:17:23 -0600

    added nojekyll file to deploy so files that start with a . are included in gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
@@ -9,5 +11,17 @@ export default defineConfig({
   output: "static",
   site: "https://putnam3d.github.io/",
   base: "/",
-  integrations: [react(),tailwind()]
+  integrations: [react(),tailwind()],
+  vite: {
+    plugins: [
+      {
+        name: 'add-nojekyll',
+        closeBundle() {
+          const outDir = join(process.cwd(), 'dist');
+          writeFileSync(join(outDir, '.nojekyll'), '');
+          console.log('.nojekyll added to dist/');
+        },
+      },
+    ],
+  },
 });
